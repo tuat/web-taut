484677c022aa257cb2a5734efe28212a62585316
Zeuxis Lo <seekstudio@gmail.com>
2014-12-12 23:30:18 +0800

    show record in index page

--- taut/routes/index.py
+++ taut/routes/index.py
@@ -1,7 +1,15 @@
 from flask import Blueprint
+from flask import render_template, request
+from ..models import ListMedia
+from ..helpers.value import force_integer, fill_with_users
 
 blueprint = Blueprint('index', __name__)
 
 @blueprint.route('/')
 def index():
-    return "Coming Soon"
+    page        = force_integer(request.args.get('page', 1), 0)
+    list_medias = ListMedia.query.filter_by(status='show').order_by(ListMedia.create_at.desc()).paginate(page)
+
+    list_medias.items = fill_with_users(list_medias.items)
+
+    return render_template('index.html', list_medias=list_medias)
