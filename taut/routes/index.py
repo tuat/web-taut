bbd510fca415f39e1c2ab7d91677ca0077415f4e
Zeuxis Lo <seekstudio@gmail.com>
2014-12-13 18:20:34 +0800

    improve index.index layout, add social button and more information

--- taut/routes/index.py
+++ taut/routes/index.py
@@ -1,6 +1,7 @@
+from random import choice
 from flask import Blueprint
 from flask import render_template, request, redirect, url_for, flash
-from ..models import ListMedia
+from ..models import ListMedia, db
 from ..forms import SigninForm, SignupForm
 from ..helpers.account import logout_user, login_user, require_user
 from ..helpers.value import force_integer, fill_with_list_users
@@ -9,12 +10,14 @@ blueprint = Blueprint('index', __name__)
 
 @blueprint.route('/')
 def index():
-    page        = force_integer(request.args.get('page', 1), 0)
-    list_medias = ListMedia.query.filter_by(status='show').order_by(ListMedia.create_at.desc()).paginate(page)
+    page         = force_integer(request.args.get('page', 1), 0)
+    list_medias  = ListMedia.query.filter_by(status='show').order_by(ListMedia.create_at.desc()).paginate(page)
+    total_medias = ListMedia.query.filter_by(status='show').count()
+    random_media = ListMedia.query.filter_by(status='show').order_by(db.func.random()).offset(0).limit(20).first()
 
     list_medias.items = fill_with_list_users(list_medias.items)
 
-    return render_template('index.html', list_medias=list_medias)
+    return render_template('index.html', list_medias=list_medias, total_medias=total_medias, random_media=random_media)
 
 @blueprint.route('/logout')
 @require_user
