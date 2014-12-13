4133fdc51ff6d25bf63c7d468588ad80f9fb0683
Zeuxis Lo <seekstudio@gmail.com>
2014-12-13 16:41:02 +0800

    move check token code to require_token decorator

--- taut/helpers/api.py
+++ taut/helpers/api.py
@@ -1,5 +1,16 @@
 import functools
-from flask import jsonify, g
+from flask import jsonify, g, request
+
+def require_token(method):
+    @functools.wraps(method)
+    def wrapper(*args, **kwargs):
+        token = request.args.get('token', None)
+
+        if not token:
+            return json_error(400, 'Please provide token')
+
+        return method(*args, **kwargs)
+    return wrapper
 
 def json_error(status, message):
     message = {
--- taut/routes/api/main.py
+++ taut/routes/api/main.py
@@ -2,13 +2,13 @@ from flask import Blueprint
 from flask import render_template, request, url_for, jsonify
 from ...models import ListMedia
 from ...helpers.value import force_integer, fill_with_list_users
-from ...helpers.api import json_error
+from ...helpers.api import require_token, json_error
 
 blueprint = Blueprint('api_main', __name__)
 
 @blueprint.route('/')
+@require_token
 def index():
-    token       = request.args.get('token', None)
     page        = force_integer(request.args.get('page', 1), 0)
     list_medias = ListMedia.query.filter_by(status='show').order_by(ListMedia.create_at.desc()).paginate(page)
 
@@ -17,12 +17,9 @@ def index():
     prev_page = url_for('api_main.index', page=page-1, _external=True) if list_medias.has_prev else None
     next_age  = url_for('api_main.index', page=page+1, _external=True) if list_medias.has_next else None
 
-    if not token:
-        return json_error(400, 'Please provide token')
-    else:
-        return jsonify(
-            items = [media.to_json(media.user) for media in list_medias.items],
-            prev  = prev_page,
-            next  = next_age,
-            count = list_medias.total
-        )
+    return jsonify(
+        items = [media.to_json(media.user) for media in list_medias.items],
+        prev  = prev_page,
+        next  = next_age,
+        count = list_medias.total
+    )
