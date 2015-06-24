9c2a875bdcb83117f80af15b2519268edc35ccf4
Zeuxis Lo <seekstudio@gmail.com>
2015-06-24 12:32:02 +0800

    add search screen name function in list user in admin view

--- taut/routes/admin/list_user.py
+++ taut/routes/admin/list_user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from ...models import ListUser
 from ...helpers.account import require_admin
 from ...helpers.value import force_integer
@@ -9,7 +9,20 @@ blueprint = Blueprint('admin_list_user', __name__)
 @blueprint.route('/index')
 @require_admin
 def index():
-    page       = force_integer(request.args.get('page', 1), 0)
-    list_users = ListUser.query.order_by(ListUser.create_at.desc()).paginate(page)
+    page        = force_integer(request.args.get('page', 1), 0)
+    screen_name = request.args.get('screen_name')
 
-    return render_template('admin/list_user/index.html', list_users=list_users)
+    query = ListUser.query
+    query = query.filter_by(screen_name=screen_name) if screen_name else query
+
+    list_users  = query.order_by(ListUser.create_at.desc()).paginate(page)
+    screen_name = screen_name if screen_name else ""
+
+    return render_template('admin/list_user/index.html', list_users=list_users, screen_name=screen_name)
+
+@blueprint.route('/search', methods=['POST'])
+@require_admin
+def search():
+    screen_name = request.form['screen_name']
+
+    return redirect(url_for('admin_list_user.index', screen_name=screen_name))
