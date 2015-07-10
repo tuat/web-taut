adfee02717b2047f79e5c9cbf002e2ed1514a6bc
Zeuxis Lo <seekstudio@gmail.com>
2015-07-10 13:00:21 +0800

    trying to fix can not generate the pages cause by url_for method throw BuildError

--- taut/commands/sitemap.py
+++ taut/commands/sitemap.py
@@ -3,6 +3,7 @@
 from os import path
 from datetime import datetime
 from dateutil import tz
+from time import sleep
 from werkzeug.routing import BuildError
 from flask import g, current_app, url_for, render_template
 from ..models import ListMedia, ListUser
@@ -16,54 +17,56 @@ class Sitemap(BaseCommand):
 
         self.logger.info("Sitemap")
 
-    def generate_media(self, offset=0, limit=10000):
-        list_medias = ListMedia.query.order_by(ListMedia.create_at.desc()).offset(offset).limit(limit).all()
+    def generate_media(self, offset, limit):
+        self.logger.info("# Generate media ({0}, {1})".format(offset, limit))
 
-        pages = []
+        list_medias = ListMedia.query.filter(ListMedia.status == 'show').order_by(ListMedia.create_at.desc()).offset(offset).limit(limit).all()
+        media_pages = []
 
         for list_media in list_medias:
-            if list_media.hash_id and list_media.status == 'show':
-                try:
-                    pages.append({
-                        'url'      : url_for_media_detail(list_media, _external=True),
-                        'image'    : thumb(list_media.media_url, 500, 500),
-                        'create_at': list_media.create_at.replace(tzinfo=tz.tzlocal()).isoformat(),
-                    })
-                except BuildError:
-                    pass
+            if list_media.hash_id:
+                media_pages.append({
+                    'url'      : url_for_media_detail(list_media, _external=True),
+                    'image'    : thumb(list_media.media_url, 500, 500),
+                    'create_at': list_media.create_at.replace(tzinfo=tz.tzlocal()).isoformat(),
+                })
 
-        sitemap   = render_template('sitemap/media.xml', pages=pages)
-        save_path = path.join(current_app.static_folder, 'sitemap-media.xml')
+        self.logger.info("---> total pages: {0}".format(len(media_pages)))
 
-        self.logger.info("===> creating sitemap-media.xml to {0}".format(save_path))
+        if media_pages:
+            sitemap   = render_template('sitemap/media.xml', pages=media_pages)
+            save_path = path.join(current_app.static_folder, 'sitemap-media.xml')
 
-        with open(save_path, 'w') as f:
-            f.write(sitemap)
-            f.close()
+            self.logger.info("---> creating sitemap-media.xml to {0}".format(save_path))
+
+            with open(save_path, 'w') as f:
+                f.write(sitemap)
+                f.close()
 
     def generate_profile(self, offset, limit):
-        list_users = ListUser.query.order_by(ListUser.create_at.desc()).offset(offset).limit(limit).all()
+        self.logger.info("# Generate media ({0}, {1})".format(offset, limit))
 
-        pages = []
+        list_users  = ListUser.query.order_by(ListUser.create_at.desc()).offset(offset).limit(limit).all()
+        media_pages = []
 
         for list_user in list_users:
-            try:
-                pages.append({
-                    'url'      : url_for('profile.index', screen_name=list_user.screen_name, _external=True),
-                    'image'    : list_user.profile_image_url,
-                    'create_at': list_user.create_at.replace(tzinfo=tz.tzlocal()).isoformat(),
-                })
-            except BuildError:
-                pass
+            media_pages.append({
+                'url'      : url_for('profile.index', screen_name=list_user.screen_name, _external=True),
+                'image'    : list_user.profile_image_url,
+                'create_at': list_user.create_at.replace(tzinfo=tz.tzlocal()).isoformat(),
+            })
 
-        sitemap   = render_template('sitemap/profile.xml', pages=pages)
-        save_path = path.join(current_app.static_folder, 'sitemap-profile.xml')
+        self.logger.info("---> total pages: {0}".format(len(media_pages)))
 
-        self.logger.info("===> creating sitemap-profile.xml to {0}".format(save_path))
+        if media_pages:
+            sitemap   = render_template('sitemap/profile.xml', pages=media_pages)
+            save_path = path.join(current_app.static_folder, 'sitemap-profile.xml')
 
-        with open(save_path, 'w') as f:
-            f.write(sitemap)
-            f.close()
+            self.logger.info("---> creating sitemap-profile.xml to {0}".format(save_path))
+
+            with open(save_path, 'w') as f:
+                f.write(sitemap)
+                f.close()
 
     def make(self, offset=0, limit=10000):
         self.generate_media(offset, limit)
