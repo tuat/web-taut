9eec5c16b36df83dfbd19f225ef689c57d9b4b88
Zeuxis Lo <seekstudio@gmail.com>
2015-07-26 16:14:03 +0800

    change check not found script using thread+queue not only process

--- taut/commands/check_not_found.py
+++ taut/commands/check_not_found.py
@@ -1,40 +1,38 @@
 # coding: utf-8
 
 import requests
+import threading
+import Queue
 from os import path
 from time import sleep
 from itertools import izip_longest
-from multiprocessing import cpu_count
-from multiprocessing.dummy import Pool
 from .base import BaseCommand
 from ..models import db, page_query, ListMedia
 from ..helpers.watcher import Watcher
 
 class CheckNotFound(BaseCommand):
 
-    not_found_ids = []
-
     def __init__(self, logger=None):
-        self.logger = self.get_logger() if logger is None else logger
+        self.logger        = self.get_logger() if logger is None else logger
+        self.not_found_ids = []
+        self.url_queue     = Queue.Queue()
 
         self.logger.info("CheckNotFound")
 
-    def grouper(self, n, iterable, fillvalue=None):
-        "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-
-        args = [iter(iterable)] * n
-        return izip_longest(fillvalue=fillvalue, *args)
-
     def make(self):
         Watcher()
+        for i in range(5):
+            t = threading.Thread(target=self.worker)
+            t.daemon = True
+            t.start()
 
-        pool = Pool(cpu_count())
+        for media in page_query(db.session.query(ListMedia)):
+            self.url_queue.put({
+                'id'       : media.id,
+                'media_url': media.media_url
+            })
 
-        for media in self.grouper(100, page_query(db.session.query(ListMedia))):
-            pool.map(self.check, media)
-
-        pool.close()
-        pool.join()
+        self.url_queue.join()
 
         flush_count = 0
         for media in page_query(db.session.query(ListMedia).filter(ListMedia.id.in_(self.not_found_ids))):
@@ -52,15 +50,21 @@ class CheckNotFound(BaseCommand):
 
         self.logger.info("--> Finished")
 
+    def worker(self):
+        while True:
+            media = self.url_queue.get()
+            self.check(media)
+            self.url_queue.task_done()
+
     def check(self, media):
         try:
-            r = requests.head(media.media_url, timeout=3)
+            r = requests.head(media['media_url'], timeout=3)
 
             if r.status_code != 200:
-                self.logger.info("--> [{0}] - {1}".format(r.status_code, media.media_url))
-                self.not_found_ids.append(media.id)
+                self.logger.info("--> [{0}] - {1: >8} - {2}".format(r.status_code, media['id'], media['media_url']))
+                self.not_found_ids.append(media['id'])
         except:
-            self.logger.info("--> Error - {0}".format(media.media_url))
-            self.not_found_ids.append(media.id)
+            self.logger.info("--> [Err] - {0: >8} - {1}".format(media['id'], media['media_url']))
+            self.not_found_ids.append(media['id'])
 
         sleep(0.005)
