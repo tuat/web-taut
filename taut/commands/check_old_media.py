c1335f37a5db19b811309b9e6061abfcc8ba08f9
Zeuxis Lo <seekstudio@gmail.com>
2015-07-28 19:53:47 +0800

    fix errno not exists

--- taut/commands/check_old_media.py
+++ taut/commands/check_old_media.py
@@ -56,7 +56,7 @@ class CheckOldMedia(BaseCommand):
                         'link': media.media_url
                     })
             except DatabaseError as e:
-                self.logger.info('--->---> Failed - {0} - {1}', e.errno, e.strerror)
+                self.logger.info('--->---> Failed - {0}', e)
                 self.logger.info('--->---> Redo method again')
                 self.make()
                 return None
@@ -101,7 +101,7 @@ class CheckOldMedia(BaseCommand):
 
                         db.session.commit()
                 except DatabaseError as e:
-                    self.logger.info('--->---> Failed - {0} - {1}', e.errno, e.strerror)
+                    self.logger.info('--->---> Failed - {0}', e)
                     self.logger.info('--->---> Redo method again')
                     self.make()
                     return None
