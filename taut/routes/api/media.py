654ab17fbcf12a8b9adf096915274754248de6f0
Zeuxis Lo <seekstudio@gmail.com>
2015-07-30 13:53:42 +0800

    fix media/detail api can not work when list media id is not numeric

--- taut/routes/api/media.py
+++ taut/routes/api/media.py
@@ -19,7 +19,7 @@ def index(list_media_id):
     user_medias = ListMedia.query.filter_by(list_user_id=list_media.list_user_id, status='show').order_by(ListMedia.create_at.desc()).offset(0).limit(12).all()
     user_medias = fill_with_list_users(user_medias)
 
-    comments = Comment.query.filter_by(list_media_id=list_media_id).order_by(Comment.create_at.asc()).all()
+    comments = Comment.query.filter_by(list_media_id=list_media.id).order_by(Comment.create_at.asc()).all()
     comments = fill_with_accounts(comments)
 
     return jsonify(
